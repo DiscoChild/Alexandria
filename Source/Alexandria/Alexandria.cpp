567795d22ff8fe2d8df91919b91b49306716238d
DiscoChild <discochild@gmail.com>
2017-02-22 23:19:44 -0600

    Initial commit

---
+++ Source/Alexandria/Alexandria.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "Alexandria.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Alexandria, "Alexandria" );
+ 
---
+++ Source/Alexandria/Alexandria.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __ALEXANDRIA_H__
+#define __ALEXANDRIA_H__
+
+#include "EngineMinimal.h"
+
+#endif
